c05ea5d86967989cd875459a817b66aa23519d16
Vasily Yurchenko <yvv@atb-e.ru>
2023-05-10 11:51:13 +0300

    transcript.py was fixed and tested.

--- transcript.py
+++ transcript.py
@@ -5,24 +5,31 @@ Created on Feb 27, 2023
 '''
 
 def main():
-    input_file_path_name = "PBe1.vtt"
-    output_file_path_name = "PBe1.out"
+    input_file_path_name = "PBe2.vtt"
+    output_file_path_name = "PBe2.out"
 
     # rfile = open(input_file_path_name, "r")
     wfile = open(output_file_path_name, "w")
-    
+
     with open(input_file_path_name, "r") as rfile:
     # for i in range(0,100):
         # s = rfile.readline()
         for s in rfile:
             if (str(s).find("WEBVTT", 0, len(s)) != -1):
                 continue
-            
+
             if (s.find("--", 0, len(s)) != -1):
                 continue
-        
-            print("{}".format(s)),
-            wfile.write("{}".format(s))
+
+            if (len(s) > 1 and s[len(s) - 1] == "\n"):
+                print("{}".format(s[:len(s) - 1])),
+                wfile.write("{} ".format(s[:len(s) - 1]))
+            else:
+                print("{}".format(s)),
+                wfile.write("{}".format(s))
+
+            # print("{}".format(len(s)))
+
         # rfile.write("{}\n");
     rfile.close();
     wfile.close();
